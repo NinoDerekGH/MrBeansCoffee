312328f3d6f90bd1dce80ee3997ec9effe597271
NiñoDerek <cabaitanneo@gmail.com>
2022-07-21 20:51:14 +0800

    edited
    edited urls.py path

--- MrBeans/urls.py
+++ MrBeans/urls.py
@@ -3,10 +3,10 @@ from . import views
 
 urlpatterns = [
     path('', views.home_out, name="home_out"),
-    path('shop/', views.shop_out, name="shop_out"),
-    path('faqs/', views.faqs_out, name="faqs_out"),
-    path('about/', views.about_out, name="about_out"),
-    path('contact/', views.contact_out, name="contact_out"),
+    path('shop_out/', views.shop_out, name="shop_out"),
+    path('faqs_out/', views.faqs_out, name="faqs_out"),
+    path('about_out/', views.about_out, name="about_out"),
+    path('contact_out/', views.contact_out, name="contact_out"),
     path('home_in/', views.home_in, name="home_in"),
     path('shop_in/', views.shop_in, name="shop_in"),
     path('faqs_in/', views.faqs_in, name="faqs_in"),
