4d30f6bf1a441f6ddba6034f33810a606a070685
NiñoDerek <cabaitanneo@gmail.com>
2022-07-21 20:44:03 +0800

    updated
    added in and out folders

--- MrBeans/urls.py
+++ MrBeans/urls.py
@@ -2,10 +2,17 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home, name="home"),
-    path('shop/', views.shop, name="shop"),
-    path('faqs/', views.faqs, name="faqs"),
-    path('about/', views.about, name="about"),
-    path('contact/', views.contact, name="contact"),
-    path('cart/', views.cart, name="cart"),
+    path('', views.home_out, name="home_out"),
+    path('shop/', views.shop_out, name="shop_out"),
+    path('faqs/', views.faqs_out, name="faqs_out"),
+    path('about/', views.about_out, name="about_out"),
+    path('contact/', views.contact_out, name="contact_out"),
+    path('home_in/', views.home_in, name="home_in"),
+    path('shop_in/', views.shop_in, name="shop_in"),
+    path('faqs_in/', views.faqs_in, name="faqs_in"),
+    path('about_in/', views.about_in, name="about_in),
+    path('contact_in/', views.contact_in, name="contact_in"),
+
+
+    path('cart_in/', views.cart_in, name="cart_in"),
 ]
--- MrBeans/views.py
+++ MrBeans/views.py
@@ -1,26 +1,46 @@
 from django.shortcuts import render
 
 
-def home(request):
-    return render(request, 'store/non_user/home.html')
+def home_out(request):
+    return render(request, 'store/user_out/home_out.html')
 
 
-def shop(request):
-    return render(request, 'store/non_user/shop.html')
+def shop_out(request):
+    return render(request, 'store/user_out/shop_out.html')
 
 
-def faqs(request):
-    return render(request, 'store/non_user/faqs.html')
+def faqs_out(request):
+    return render(request, 'store/user_out/faqs_out.html')
 
 
-def about(request):
-    return render(request, 'store/non_user/about.html')
+def about_out(request):
+    return render(request, 'store/user_out/about_out.html')
 
 
-def contact(request):
-    return render(request, 'store/non_user/contact.html')
+def contact_out(request):
+    return render(request, 'store/user_out/contact_out.html')
 
 
-def cart(request):
-    return render(request, 'store/user_interface/cart.html')
+def home_in(request):
+    return render(request, 'store/user_in/home_in.html')
+
+
+def shop_in(request):
+    return render(request, 'store/user_in/shop_in.html')
+
+
+def faqs_in(request):
+    return render(request, 'store/user_in/faqs_in.html')
+
+
+def about_in(request):
+    return render(request, 'store/user_in/about_in.html')
+
+
+def contact_in(request):
+    return render(request, 'store/user_in/contact_in.html')
+
+
+def cart_in(request):
+    return render(request, 'store/user_in/cart_in.html')
 
